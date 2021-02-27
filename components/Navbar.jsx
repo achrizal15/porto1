18adb147a58c08ea36c1c85af564de77b1170dd2
achrizal15 <achrizal15@gmail.com>
2021-02-28 00:33:32 +0700

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,17 +6,17 @@ export default function Navbar() {
     <div className="bg-blue-100 md:sticky hidden md:block z-50  top-0">
       <div className="flex items-center justify-between px-7 antialiased py-2">
         <p className="font-bold italic text-white text-xl font-mono">
-          <Smooth.Link to="home" smooth>Welcome To My Portfolio</Smooth.Link>
+          <Smooth.Link to="home" smooth className='cursor-pointer'>Welcome To My Portfolio</Smooth.Link>
         </p>
         <div className="flex items-center font-mono text-md">
           <div className="px-2 text-blue-400 hover:text-white">
-            <Smooth.Link smooth to="about">About</Smooth.Link>
+            <Smooth.Link smooth className='cursor-pointer' to="about">About</Smooth.Link>
           </div>
           <div className="px-2 text-blue-400 hover:text-white">
-            <Smooth.Link smooth to="project">Project</Smooth.Link>
+            <Smooth.Link smooth className='cursor-pointer' to="project">Project</Smooth.Link>
           </div>
           <div className="px-2 text-blue-400 hover:text-white">
-            <Smooth.Link smooth to="contact">Contact</Smooth.Link>
+            <Smooth.Link smooth className='cursor-pointer' to="contact">Contact</Smooth.Link>
           </div>
         </div>
       </div>
