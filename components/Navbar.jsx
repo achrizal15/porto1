e35687863674cc055335f18d685d566d6fc31f57
achrizal15 <achrizal15@gmail.com>
2021-02-28 00:31:41 +0700

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import Link from "next/link";
+import * as Smooth from "react-scroll";
 
 export default function Navbar() {
   return (
     <div className="bg-blue-100 md:sticky hidden md:block z-50  top-0">
       <div className="flex items-center justify-between px-7 antialiased py-2">
         <p className="font-bold italic text-white text-xl font-mono">
-          <Link href="#home">Welcome To My Portfolio</Link>
+          <Smooth.Link to="home" smooth>Welcome To My Portfolio</Smooth.Link>
         </p>
         <div className="flex items-center font-mono text-md">
           <div className="px-2 text-blue-400 hover:text-white">
-            <Link href="#about">About</Link>
+            <Smooth.Link smooth to="about">About</Smooth.Link>
           </div>
           <div className="px-2 text-blue-400 hover:text-white">
-            <Link href="#project">Project</Link>
+            <Smooth.Link smooth to="project">Project</Smooth.Link>
           </div>
           <div className="px-2 text-blue-400 hover:text-white">
-            <Link href="#contact">Contact</Link>
+            <Smooth.Link smooth to="contact">Contact</Smooth.Link>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../components/Layout";
 import About from "./source/About";
 import Contact from "./source/Contact";
@@ -6,6 +6,11 @@ import Home from "./source/Home";
 import Project from "./source/Project";
 
 export default function index() {
+  useEffect(() => {
+    window.addEventListener('scroll', (event) => {
+      console.log('sa')
+    });
+  }, []);
   return (
     <Layout>
       <Home />
